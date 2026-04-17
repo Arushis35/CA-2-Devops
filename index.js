3c99555f239b863270685db1568380e6546f33dd
Arushis35 <arushi03singh02gmail.com>
2026-04-17 14:39:47 +0530

    added health/

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
-
 const app = express();
 const PORT = 3000;
 
+// Health route
+app.get("/health", (req, res) => {
+  res.json({ status: "OK", message: "Working 🚀" });
+});
+
+// Home route
 app.get("/", (req, res) => {
-  res.send("Hello World 🚀");
+  res.send("Main Server Running 🚀");
 });
 
 app.listen(PORT, () => {
